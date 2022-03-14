481eec6e1c1c96baa1033ac93cdc303789114130
arthurisvi <83462514+arthurisvi@users.noreply.github.com>
2022-03-13 23:12:37 -0300

    initial commit

---
+++ src/main/java/com/scccartola/apiscc/ApiSccApplication.java
@@ -0,0 +1,13 @@
+package com.scccartola.apiscc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSccApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scccartola/apiscc/ApiSccApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scccartola.apiscc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
