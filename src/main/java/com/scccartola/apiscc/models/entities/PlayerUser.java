3189a5b54687e57b1a06336b3d09b869bf38a755
arthurisvi <83462514+arthurisvi@users.noreply.github.com>
2022-03-18 00:32:36 -0300

    fix: making object serializable with empty constructor

--- src/main/java/com/scccartola/apiscc/models/entities/PlayerUser.java
+++ src/main/java/com/scccartola/apiscc/models/entities/PlayerUser.java
@@ -5,12 +5,14 @@ import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @Entity
+@NoArgsConstructor
 @Table(name = "tb_player_user")
 public class PlayerUser extends User {
     private String whatsappNum;
